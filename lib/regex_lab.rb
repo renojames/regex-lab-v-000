6ede66e11b0424d1009d1dcc40b704541999e992
Reno Sorensen <renojames19@gmail.com>
2018-03-10 22:49:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,6 +19,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/^"[(]?\d{3}[)]?[\s]?[-]?\d{3}[\s]?[-]?\d{4}"$/).all? ? true : false
+  if phone.scan(/^"[(]?\d{3}[)]?[\s]?[-]?\d{3}[\s]?[-]?\d{4}"$/) ? true : false
   end
 end
