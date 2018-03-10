e6e8e413c85f8f33a4e41d87130852532d0c0ccb
Reno Sorensen <renojames19@gmail.com>
2018-03-10 22:52:53 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,17 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z].*\W\z/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/[(]?\d{3}[)]?[\s]?[-]?\d{3}[\s]?[-]?\d{4}/)
+    true
+  else
+    false
+  end
 end
