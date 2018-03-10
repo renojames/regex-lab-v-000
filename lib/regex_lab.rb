28b3ef78314622adfef91c72a4a3a4731e739329
Reno Sorensen <renojames19@gmail.com>
2018-03-10 22:15:48 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/"\d{10}.*"/)
+  if phone.grep(/"\d{10}.*"/)
     true
   else
     false
