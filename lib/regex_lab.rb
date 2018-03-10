9f816f6b95abcf43483c2114088c9016533b8483
Reno Sorensen <renojames19@gmail.com>
2018-03-10 22:47:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/^"[(]?\d{3}[)]?[\s]?[-]?\d{3}[\s]?[-]?\d{4}"$/)
+  if phone.scan(/^"[(]?\d{3}[)]?[\s]?[-]?\d{3}[\s]?[-]?\d{4}"$/)
     true
   else
     false
